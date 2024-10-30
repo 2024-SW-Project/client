4a3e296a4741c49f35207bd448b9b05ca99bcbf1
“2seb2” <yuo231@naver.com>
2024-10-30 11:12:51 +0900

    feat: React Router 추가 및 CSS 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import './App.css';
+import SubwaySearchPage from './pages/SubwaySearchPage';
+import Login from './pages/Login'
+import Header from './components/Header'
 
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const RootLayout = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>!Vite + React!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header /> {/* 고정된 Header */}
+      <Outlet />  {/* 라우팅된 페이지가 렌더링될 위치 */}
     </>
-  )
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />, // RootLayout이 최상위 요소
+    children: [
+      {
+        path: '/',
+        element: <SubwaySearchPage />
+      },
+      {
+        path: '/login',
+        element: <Login /> // '/login' 경로
+      },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <RouterProvider router={router} />
+  );
 }
 
-export default App
+export default App;
