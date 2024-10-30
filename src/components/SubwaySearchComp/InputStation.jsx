87b4e4668b991c583e42c3ba5599e366b5e82c9a
“2seb2” <yuo231@naver.com>
2024-10-31 03:44:41 +0900

    Feat: InputStation 컴포넌트 추가

---
+++ src/components/SubwaySearchComp/InputStation.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import styled from "styled-components";
+import { FaLocationArrow } from "react-icons/fa6";
+import { FaFlag } from "react-icons/fa6";
+
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    border: 1px solid #C0C0C0;
+    border-radius: 20px;
+    padding: 0.5rem 1rem;
+    width: 100%;
+    box-sizing: border-box;
+`;
+
+// 인풋창 내부의 아이콘(출발아이콘)
+const StartIcon = styled(FaLocationArrow)`
+    color: #4A6CF7;
+    margin-right: 0.5rem;
+`;
+
+// 인풋창 내부의 아이콘(도착아이콘)
+const ArrivedIcon = styled(FaFlag)`
+    color: #4A6CF7;
+    margin-right: 0.5rem;
+`;
+
+// 인풋창
+const Input = styled.input`
+    border: none;
+    outline: none;
+    font-size: 14px;
+    color: #666271;
+    width: 100%;
+    
+    &::placeholder {
+        color: #C0C0C0;
+    }
+`;
+
+const InputStation = ({ placeholder, value, onChange }) => {
+    return (
+        <Container>
+            {placeholder === '출발역' ? <StartIcon /> : <ArrivedIcon />}
+            <Input
+                placeholder={placeholder}
+                value={value}
+                onChange={(e) => onChange(e.target.value)}
+            />
+        </Container>
+    );
+};
+
+export default InputStation;
