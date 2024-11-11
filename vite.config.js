9c525a9cfc791d9be0b2468f73618499037642be
“2seb2” <yuo231@naver.com>
2024-11-11 10:10:37 +0900

    Chore: 개발 서버 시작 시 자동으로 열릴 경로 설정

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import svgr from 'vite-plugin-svgr'; // svgr 추가
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  server: {
+    open: '/subway/search' // 개발 서버 시작시 자동으로 열릴 경로
+  }
 })
